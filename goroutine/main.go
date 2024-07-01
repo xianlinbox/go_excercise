2106b5b96359c3c96be30b80e86e6c0898d358a1
xianning liu <xianlinbox@gmail.com>
2024-07-01 10:40:58 -0500

    add go routine and channel'

---
+++ goroutine/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+ 
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://xxsfdfsd.com",
+	}
+
+	c := make(chan string)
+
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for i:=0; i < len(links); i++ {
+		fmt.Println(<-c, "finished checking")
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		c <- link 
+		fmt.Println(link, "might be down!")
+		return
+	}
+	c <- link 
+	fmt.Println(link, " is up")
+}
